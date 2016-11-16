30dcbc821ea43fdb68fc1c49db7fb546d606203b
nyutal <yutal.nadav@gmail.com>
2016-11-16 22:12:16 +0200

    Add files via upload

---
+++ Ball.java
@@ -0,0 +1,38 @@
+package homework0;
+
+/**
+ * A simple object that has a volume.
+ */
+public class Ball {
+	
+    /**
+     * @requires volume > 0
+     * @modifies this
+     * @effects Creates and initializes new Ball object with the specified
+     *  		volume.
+     */
+    public Ball(double volume) {
+		// TODO: Add your code here
+		
+    }
+
+
+	/**
+	 * @requires volume > 0
+	 * @modifies this
+	 * @effects Sets the volume of the Ball.
+	 */
+	public void setVolume(double volume) {
+		// TODO: Add your code here
+		
+	}
+
+
+    /**
+     * @return the volume of the Ball.
+     */
+    public double getVolume() {
+		// TODO: Add your code here
+		
+    }
+}
---
+++ BallContainer.java
@@ -0,0 +1,79 @@
+package homework0;
+
+/**
+ * A container that can be used to contain Balls. A given Ball may only
+ * appear in a BallContainer once.
+ */
+public class BallContainer {
+
+    /**
+     * @effects Creates a new BallContainer.
+     */
+    public BallContainer() {
+		//TODO: Add your code here
+		
+    }
+
+
+    /**
+     * @modifies this
+     * @effects Adds ball to the container.
+     * @return true if ball was successfully added to the container,
+     * 		   i.e. ball is not already in the container; false otherwise.
+     */
+    public boolean add(Ball ball) {
+		// TODO: Add your code here
+		
+    }
+
+
+    /**
+     * @modifies this
+     * @effects Removes ball from the container.
+     * @return true if ball was successfully removed from the container,
+     * 		   i.e. ball is actually in the container; false otherwise.
+     */
+    public boolean remove(Ball ball) {
+		// TODO: Add your code here
+		
+    }
+
+
+    /**
+     * @return the volume of the contents of the container, i.e. the
+     * 		   total volume of all Balls in the container.
+     */
+    public double getVolume() {
+		// TODO: Add your code here
+		
+    }
+
+
+    /**
+     * @return the number of Balls in the container.
+     */
+    public int size() {
+		// TODO: Add your code here
+		
+    }
+
+
+    /**
+     * @modifies this
+     * @effects Empties the container, i.e. removes all its contents.
+     */
+    public void clear() {
+		// TODO: Add your code here
+		
+    }
+
+
+    /**
+     * @return true if this container contains ball; false, otherwise.
+     */
+    public boolean contains(Ball ball) {
+		// TODO: Add your code here
+	
+    }
+
+}
